4bd1c59ea24105bfd2d8fd5514b8404e3543108e
DaviErlon <163437822+DaviErlon@users.noreply.github.com>
2025-02-21 06:08:35 -0300

    Adição de arquivos auxiliares para o início do projeto

---
+++ inc/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+typedef struct{
+    uint8_t R;
+    uint8_t G;
+    uint8_t B;
+} led;
+
+uint32_t valor_rgb(uint8_t, uint8_t, uint8_t);
+void set_led(uint8_t, uint8_t, uint8_t, uint8_t);
+void clear_leds(void);
+void print_leds(void);
+
+#endif
--- main.c
+++ main.c
@@ -1,57 +1,77 @@
-#include <stdio.h>
 #include "pico/stdlib.h"
-#include "hardware/i2c.h"
-#include "hardware/pio.h"
+#include "hardware/pwm.h"
+#include "hardware/adc.h"
+#include "hardware/clocks.h"
+#include "ws2812b.pio.h"
+#include "inc/ssd1306.h"
+#include "inc/main.h"
 
-// I2C defines
-// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define I2C_PORT i2c0
-#define I2C_SDA 8
-#define I2C_SCL 9
+#define LED_PIN 7
+#define LED_COUNT 25
+#define I2C_PORT i2c1
+#define I2C_SDA 14
+#define I2C_SCL 15
+#define endereco 0x3C
 
-#include "blink.pio.h"
-
-void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
-    blink_program_init(pio, sm, offset, pin);
-    pio_sm_set_enabled(pio, sm, true);
-
-    printf("Blinking pin %d at %d Hz\n", pin, freq);
-
-    // PIO counter program takes 3 more cycles in total than we pass as
-    // input (wait for n + 1; mov; jmp)
-    pio->txf[sm] = (125000000 / (2 * freq)) - 3;
-}
-
-
+static PIO pio;
+static uint sm;
+volatile led matriz_led[LED_COUNT] = {0};
 
 int main()
-{
-    stdio_init_all();
+{   
+    // configurações para a PIO que controla a matriz de leds
+    pio = pio0; 
+    set_sys_clock_khz(128000, false);
+    uint offset = pio_add_program(pio, &ws2812b_program);
+    sm = pio_claim_unused_sm(pio, true);
+    ws2812b_program_init(pio, sm, offset, LED_PIN);
 
-    // I2C Initialisation. Using it at 400Khz.
-    i2c_init(I2C_PORT, 400*1000);
-    
+    // configurações para o i2c que controla a tela OLED
+    i2c_init(I2C_PORT, 400 * 1000);
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
     gpio_pull_up(I2C_SDA);
     gpio_pull_up(I2C_SCL);
-    // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
+    ssd1306_t ssd;
+    ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT);
+    ssd1306_config(&ssd);
+    ssd1306_fill(&ssd, false);
+    ssd1306_send_data(&ssd);
 
-    // PIO Blinking example
-    PIO pio = pio0;
-    uint offset = pio_add_program(pio, &blink_program);
-    printf("Loaded program at %d\n", offset);
-    
-    #ifdef PICO_DEFAULT_LED_PIN
-    blink_pin_forever(pio, 0, offset, PICO_DEFAULT_LED_PIN, 3);
-    #else
-    blink_pin_forever(pio, 0, offset, 6, 3);
-    #endif
-    // For more pio examples see https://github.com/raspberrypi/pico-examples/tree/master/pio
+    while(true)
+    {
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+    }
+}
+
+// função que une os dados binários dos LEDs para emitir para a PIO
+uint32_t valor_rgb(uint8_t B, uint8_t R, uint8_t G){
+  return (G << 24) | (R << 16) | (B << 8);
+}
+
+// função que envia dados ao buffer de leds
+void set_led(uint8_t indice, uint8_t r, uint8_t g, uint8_t b){
+    if(indice < LED_COUNT){
+    matriz_led[indice].R = r;
+    matriz_led[indice].G = g;
+    matriz_led[indice].B = b;
+    }
+}
+
+// função que limpa o buffer de leds
+void clear_leds(void){
+    for(uint8_t i = 0; i < LED_COUNT; i++){
+        matriz_led[i].R = 0;
+        matriz_led[i].B = 0;
+        matriz_led[i].G = 0;
+    }
+}
+
+// função que manda os dados do buffer para os LEDs
+void print_leds(void){
+    uint32_t valor;
+    for(uint8_t i = 0; i < LED_COUNT; i++){
+        valor = valor_rgb(matriz_led[i].B, matriz_led[i].R,matriz_led[i].G);
+        pio_sm_put_blocking(pio, sm, valor);
     }
 }
